4cfa4d933c885d00d22fedce3e6d02d8410c0fb3
avdnowhere <avd4388@gmail.com>
2022-06-28 20:57:40 +0800

    Update index.js

--- src/components/HelloSection/index.js
+++ src/components/HelloSection/index.js
@@ -7,7 +7,7 @@ import Groom from '@assets/images/p-indra.jpg';
 import { styWrapper } from './styles';
 
 function HelloSection({ isInvitation }) {
-  const finalSubtitle = isInvitation ? '03 Oktober 2020, HARRIS Hotel Sentraland, Semarang' : 'Sabtu, 03 Oktober 2020';
+  const finalSubtitle = isInvitation ? '20 Ogos 2022, Kota Tinggi' : 'Sabtu, 20 Ogos 2022';
 
   return (
     <Fragment>
@@ -30,9 +30,9 @@ function HelloSection({ isInvitation }) {
                 <img src={Bride} alt="groom" className="img-responsive" loading="lazy" />
               </div>
               <div className="desc-groom">
-                <h3 className="main-font">Dinda Saraswati, S.I.Kom.</h3>
+                <h3 className="main-font">Ammy Vega Darya</h3>
                 <p className="parent-name parent-name__top">
-                  Putri Bapak Totok Somo Dipoyono <br />& Ibu Setyo Listiani
+                  Putera Jasman Aira Darya <br />& Sudarmi
                 </p>
               </div>
             </div>
@@ -47,9 +47,9 @@ function HelloSection({ isInvitation }) {
                 <img src={Groom} alt="groom" className="img-responsive" loading="lazy" />
               </div>
               <div className="desc-bride">
-                <h3 className="main-font">Indra Kusuma, S.Kom.</h3>
+                <h3 className="main-font">Norain Binti Johari</h3>
                 <p className="parent-name">
-                  Putra Bapak Bunari <br />& Ibu Suratun
+                  Puteri Johari Bin Mohammad <br />& Fatimah Binti Iran
                 </p>
               </div>
             </div>
@@ -61,16 +61,16 @@ function HelloSection({ isInvitation }) {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                <h2 className="main-font">Seputar Covid-19</h2>
+                <h2 className="main-font">Sekilas Covid-19</h2>
                 <p className="info">
-                  Berkaitan dengan kondisi pandemik saat ini, keberlangsungan acara kami mengacu pada pedoman
-                  Kementerian Kesehatan, yaitu memberlakukan pembatasan jarak dan pembatasan jumlah tamu undangan.
+                  Berkaitan dengan kondisi pandemik saat ini, keberlangsungan majlis kami akan mengikuti 
+                  prosedur operasi standard (SOP) daripada KKM, yaitu mewajibkan pemakaian pelitup muka kepada semua tamu yang hadir.
                 </p>
-                <p className="info">
-                  Maka dari itu, dengan segala kerendahan hati, kami mohon maaf jika belum bisa mengundang saudara/i
-                  dihari bahagia kami. Sekiranya do'a dari saudara/i sudah lebih dari cukup bagi kami. Semoga kita semua
-                  selalu dalam lindungan Allah SWT. Aamiin ya rabbal allamiin..
-                </p>
+//                 <p className="info">
+//                   Maka dari itu, dengan segala kerendahan hati, kami mohon maaf jika belum bisa mengundang saudara/i
+//                   dihari bahagia kami. Sekiranya do'a dari saudara/i sudah lebih dari cukup bagi kami. Semoga kita semua
+//                   selalu dalam lindungan Allah SWT. Aamiin ya rabbal allamiin..
+//                 </p>
               </div>
             </div>
           </div>
