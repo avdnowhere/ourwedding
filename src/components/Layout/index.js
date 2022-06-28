36d49cee3e7de01c585cc53f3315d7461241dace
avdnowhere <avd4388@gmail.com>
2022-06-28 20:08:57 +0800

    Update index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -14,18 +14,18 @@ function MainLayout({ children }) {
   return (
     <Fragment>
       <Helmet>
-        <title>Dinda ❤️ Indra Wedding</title>
+        <title>Ammy ❤️ Norain Wedding</title>
 
         {/* Favicon */}
         <link rel="icon" type="image/png" href={Favicon} />
 
         {/* font and SEO tags */}
-        <meta property="og:title" content="The Wedding of Dinda & Indra" />
+        <meta property="og:title" content="The Wedding of Ammy & Norain" />
         <meta property="og:image" content={IMAGE_URL} />
         <meta property="og:url" content="https://thekusuma.com" />
-        <meta property="og:site_name" content="The Wedding of Dinda & Indra" />
+        <meta property="og:site_name" content="The Wedding of Ammy & Norain" />
         <meta property="og:description" content={META_DESCRIPTION} />
-        <meta name="twitter:title" content="The Wedding of Dinda & Indra" />
+        <meta name="twitter:title" content="The Wedding of Ammy & Norain" />
         <meta name="twitter:description" content={META_DESCRIPTION} />
         <meta name="twitter:image" content={IMAGE_URL} />
         <meta name="twitter:url" content="https://thekusuma.com" />
